49655bbe2678c6d85a77c6617cfc3d644eb18089
Ali Abu Nazahat <abu.nazahat@gmail.com>
2025-07-25 16:55:53 +0000

    fully fledged rps game completed every requirement in TOP fixed scores and added a win condition that is best of 3

--- javascript.js
+++ javascript.js
@@ -1,60 +1,81 @@
+let humanScore =0, computerScore =0;
 function playGame(){ //GOTTTA ARRANGE STUFF PROPERLY IN THIS
-    console.log("Hello world")
     function getComputerChoice(){   //code to get computer's choice
         let num=Math.floor(Math.random() * 3);
+        let C_choice = "";
         if(num==0){
-            return "rock";
+            C_choice = "rock";
         }
         else if(num==1){
-            return "scissor";
+            C_choice = "scissors";
         }
-        else return "paper";
+        else{ 
+            C_choice = "paper";
+        }
+        console.log("Computer chose", C_choice);
+        return C_choice;
     }
-
+    
     function getHumanChoice(){
-        let CHOICE= prompt("Enter your choice for rps ");
-        let choice = CHOICE.toLowerCase();
-        return choice;
+        let CHOICE;
+        while (true) {
+            CHOICE = prompt("Enter rock, paper, or scissors:");
+            if (!CHOICE) continue;
+            CHOICE = CHOICE.toLowerCase();
+            if (CHOICE === "rock" || CHOICE === "paper" || CHOICE === "scissors") {
+                console.log("You chose", CHOICE);
+                return CHOICE;
+            } else {
+                alert("Invalid input! Please enter rock, paper, or scissors.");
+            }
+        }
     }
-
-
-    let humanScore =0, computerScore =0;
-
+    
     function playRound(humanChoice, computerChoice){
-        if(humanChoice == "rock" && computerChoice=="scissor"){
-            console.log("PLAYER WINS!!")
-            return humanScore; //gotta experiment with returning and incrementing the scores
-        }
-        if(humanChoice == "scissor" && computerChoice=="paper"){
-            console.log("PLAYER WINS!!")
-            return humanScore;
-        }
-        if(humanChoice == "paper" && computerChoice=="rock"){
-            console.log("PLAYER WINS!!")
-            return humanScore;
-        }
-        if(humanChoice == "rock" && computerChoice=="scissor"){
-            console.log("COMPUTER WINS!!")
-            return computerScore;
-        }
-        if(humanChoice == "rock" && computerChoice=="paper"){
-            console.log("COMPUTER WINS!!")
-            return computerScore;
-        }
-        if(humanChoice == "scissor" && computerChoice=="rock"){
-            console.log("COMPUTER WINS!!")
-            return computerScore;
-        }
         if(humanChoice == computerChoice){
             console.log("TIE!!")
         }
-    }
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        for(var i=1; i<5;i++){
-            playRound(humanSelection,computerSelection);
+        else if(humanChoice == "rock" && computerChoice=="scissors"){
+            console.log("PLAYER WINS!!")
+            humanScore++;
         }
-        console.log(huma)
-    }
+        else if(humanChoice == "scissors" && computerChoice=="paper"){
+            console.log("PLAYER WINS!!")
+            humanScore++;
+        }
+        else if(humanChoice == "paper" && computerChoice=="rock"){
+            console.log("PLAYER WINS!!")
+            humanScore++;
+        }
+        else if(humanChoice == "paper" && computerChoice=="scissors"){
+            console.log("COMPUTER WINS!!")
+            computerScore++;
+        }
+        else if(humanChoice == "rock" && computerChoice=="paper"){
+            console.log("COMPUTER WINS!!")
+            computerScore++;
+        }
+        else if(humanChoice == "scissors" && computerChoice=="rock"){
+            console.log("COMPUTER WINS!!")
+            computerScore++;
+        }
+    }   
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection,computerSelection);
+    console.log(`Your current score is ${humanScore} Computer's score is ${computerScore}`)
+}
+let i=1;
+while(humanScore < 3 && computerScore < 3){
+    console.log(`Round ${i}`)
+    playGame();
+    console.log("----------------------------");
+    i++;
+}
+console.log(`Final Score → You: ${humanScore}, Computer: ${computerScore}`);
 
-playGame();
+if (humanScore === 3) {
+    console.log("🎉 Player Wins the BEST OF 3 MATCH!");
+} else {
+    console.log("💻 Computer Wins the BEST OF 3 MATCH!");
+}
